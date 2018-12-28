0260929a51c74cbf6455e83e8a9847dc9aac1cb6
Keegan Rankin <kms.rankin@gmail.com>
2018-12-28 02:54:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// add solution here
+function theBeatlesPlay(musicians, instruments) {
+  var array = []
+  for (let i = 0; i < 4; i++) {
+    array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return array
+}
+
+function johnLennonFacts(facts) {
+  var array = []
+  let i = 0
+  while (i < facts.length) {
+    array.push(`${facts[i]}!!!`);
+    i++;
+  }
+  return array
+}
